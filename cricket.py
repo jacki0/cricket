a111e8d2798d8d689a9c5f7d8775be6279815294
jacki0 <jackio@tuta.io>
2020-09-09 02:29:07 +0300

    Edited the code style

--- cricket.py
+++ cricket.py
@@ -1,20 +1,20 @@
 import requests
 import random
 
-def get_request(extract_url):
-    ''' Sends a GET request to a random URL from the list.
-    '''
 
+def get_request(extract_url):
+    """ Sends a GET request to a random URL from the list.
+    """
     get = requests.get(extract_url)
 
     return get
 
+
 def url_extraction():
-    ''' Chooses a random URL from the list.
-    '''
-
-    urllist = []
+    """ Chooses a random URL from the list.
+    """
+    url_list = []
     with open('list.txt') as file:
-        urllist = [row.strip() for row in file]
+        url_list = [row.strip() for row in file]
 
-    return random.choice(['http://', 'https://']) + random.choice(urllist)
+    return random.choice(['http://', 'https://']) + random.choice(url_list)
