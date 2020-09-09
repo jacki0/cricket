45dddd098138fc4b5076175ae4b329bbabdc3331
jacki0 <jackio@tuta.io>
2020-09-09 18:26:49 +0300

    fix code style

--- cricket.py
+++ cricket.py
@@ -2,47 +2,44 @@ import requests
 import random
 import threading
 
-class perpetualTimer():
 
-      def __init__(self,t,hFunction):
-         self.t=t
-         self.hFunction = hFunction
-         self.thread = threading.Timer(self.t,self.handle_function)
+class PerpetualTimer():
+    def __init__(self, t, hfunction):
+        self.hFunction = hfunction
+        self.t=t
+        self.thread = threading.Timer(self.t, self.handle_function)
+        self.hFunction = hfunction
 
-      def handle_function(self):
-         self.hFunction()
-         self.thread = threading.Timer(self.t,self.handle_function)
-         self.thread.start()
+    def handle_function(self):
+        self.hFunction()
+        self.thread = threading.Timer(self.t, self.handle_function)
+        self.thread.start()
 
-      def start(self):
-         self.thread.start()
+    def start(self):
+        self.thread.start()
 
-      def cancel(self):
-         self.thread.cancel()
+    def cancel(self):
+        self.thread.cancel()
 
 
-def get_request():
+def get_request(get=None):
     """ Sends a GET request to a random URL from the list.
 
     """
     try:
         get = requests.get(url_extraction())
-    except:
-        get = 'Traceback'
-
-    return str(get)
+    finally:
+        return get
 
 
-def url_extraction():
+def url_extraction(url_list=[]):
     """ Chooses a random URL from the list.
 
     """
-    url_list = []
     with open('list.txt') as file:
         url_list = [row.strip() for row in file]
-
     return random.choice(['http://', 'https://']) + random.choice(url_list)
 
 
-t = perpetualTimer(5, get_request)
+t = PerpetualTimer(9, get_request)
 t.start()
