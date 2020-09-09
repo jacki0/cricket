e1a16cf3ddb3de6d4a23fcda7c002ecdff04f693
jacki0 <jackio@tuta.io>
2020-09-09 04:38:12 +0300

    Added requests execution timer

--- cricket.py
+++ cricket.py
@@ -1,14 +1,36 @@
 import requests
 import random
+import threading
+
+class perpetualTimer():
+
+      def __init__(self,t,hFunction):
+         self.t=t
+         self.hFunction = hFunction
+         self.thread = threading.Timer(self.t,self.handle_function)
+
+      def handle_function(self):
+         self.hFunction()
+         self.thread = threading.Timer(self.t,self.handle_function)
+         self.thread.start()
+
+      def start(self):
+         self.thread.start()
+
+      def cancel(self):
+         self.thread.cancel()
 
 
-def get_request(extract_url):
+def get_request():
     """ Sends a GET request to a random URL from the list.
-    
-    """
-    get = requests.get(extract_url)
 
-    return get
+    """
+    try:
+        get = requests.get(url_extraction())
+    except:
+        get = 'Traceback'
+
+    return str(get)
 
 
 def url_extraction():
@@ -20,3 +42,7 @@ def url_extraction():
         url_list = [row.strip() for row in file]
 
     return random.choice(['http://', 'https://']) + random.choice(url_list)
+
+
+t = perpetualTimer(5, get_request)
+t.start()
